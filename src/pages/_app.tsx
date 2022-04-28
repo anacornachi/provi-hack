c05d5dbb32e6b652bb9880b130d0f8030726a6f3
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-28 17:09:38 -0300

    🎨 improve structure and add chakra ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type {AppProps} from 'next/app';
+import {ChakraProvider, theme} from '@chakra-ui/react';
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
